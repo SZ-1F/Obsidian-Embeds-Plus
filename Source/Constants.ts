abce3f9da90e0ceb6f33326c86e4158f097e40b8
Shaizaad (SZ-1F) <52267502+SZ-1F@users.noreply.github.com>
2026-05-20 15:42:50 +1000

    Update Render Cache Versioning
    - Updated `RENDER_CACHE_VERSION` in `Constants.ts` to be dynamically assigned by ESBuild instead of manually incremented.
    - Updated `esbuild.config.mjs` to generate a random render cache version number at build time.

--- Source/Constants.ts
+++ Source/Constants.ts
@@ -6,7 +6,8 @@ export const HTML_EMBED_IFRAME_SANDBOX =
 export const FILE_MODIFY_DEBOUNCE_MS = 300;
 export const NON_BLOCKING_RENDER_TIMEOUT_MS = 100;
 export const HTML_LOAD_FAILURE_TIMEOUT_MS = 15000;
-export const RENDERED_HTML_CACHE_VERSION = 3;
+declare const __RENDER_CACHE_VERSION__: number;
+export const RENDERED_HTML_CACHE_VERSION = __RENDER_CACHE_VERSION__;
 
 const HtmlEmbedExtensions = new Set(['html', 'mhtml', 'mht', 'webarchive', 'enex']);
 const HtmlViewExtensions = new Set(['html', 'mhtml', 'mht', 'webarchive', 'enex']);
